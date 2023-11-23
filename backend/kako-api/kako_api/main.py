1bbf7ff8ec25414f54573d2612106e21487e4341
Funobu <fukids34@gmail.com>
2023-11-23 14:50:19 +0900

    [add] 加工APIの実装

--- backend/kako-api/kako_api/main.py
+++ backend/kako-api/kako_api/main.py
@@ -1,9 +1,37 @@
+import io
+from typing import Annotated
 from rembg import remove
 from PIL import Image
+from fastapi import FastAPI, UploadFile, Response
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+
+
+class RemoveCharactorImageResponse(BaseModel):
+    image: bytes
+
+
+@app.post("/")
+async def remove_charactor_image_bg(file: UploadFile):
+    charactor_file = await file.read()
+    charactor_file_image = Image.open(io.BytesIO(charactor_file))
+
+    removed_image = remove(charactor_file_image)
+    output = io.BytesIO()
+    removed_image.save(output, "PNG")
+    output_image_bytes = output.getvalue()
+
+    await file.close()
+    return Response(content=output_image_bytes, media_type="image/png")
+
 
 input_path = 'assets/sample2.jpg'
 output_path = 'assets/output_sample2.png'
 
 input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
